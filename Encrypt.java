e1f4383e1943d388a385f76f5ff72e808135e172
Aneesh Pothuru <pothuruaneesh23@gmail.com>
2021-04-11 20:21:44 -0700

    SHA256 encryption for blockchain data. Created with Java: static class

---
+++ Encrypt.java
@@ -0,0 +1,35 @@
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author Aneesh Pothuru and Edgar Villanueva
+ * @version April 2021
+ */
+public class Encrypt {
+
+    /**
+     * @param data
+     * @return byte[]
+     * @throws NoSuchAlgorithmException
+     */
+    public static byte[] getBytes(String data) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        return md.digest(data.getBytes(StandardCharsets.UTF_8));
+    }
+
+    /**
+     * @param data
+     * @return
+     */
+    public static String SHA256(String data) {
+        BigInteger number = new BigInteger(1, data.getBytes());
+        StringBuilder hex = new StringBuilder(number.toString(16));
+        while (hex.length() < 32) {
+            hex.insert(0, '0');
+        }
+        return hex.toString();
+    }
+
+}
